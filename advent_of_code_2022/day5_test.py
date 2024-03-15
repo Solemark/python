82e0c4e2e4a5ac55bb8ce0d206122336bd11f539
Solemark <larco9mason@gmail.com>
2024-03-15 21:29:13 +1000

    data isnt a constant

--- advent_of_code_2022/day5_test.py
+++ advent_of_code_2022/day5_test.py
@@ -5,7 +5,7 @@ from day5 import day5
 
 class advent_of_code_2022_tests(unittest.TestCase):
     def test_day5(self) -> None:
-        DATA: list[list[str]] = [["Z", "N"], ["M", "C", "D"], ["P"]]
+        data: list[list[str]] = [["Z", "N"], ["M", "C", "D"], ["P"]]
         MOVES: list[list[int]] = [[1, 2, 1], [3, 1, 3], [2, 2, 1], [1, 1, 2]]
         EXPECT: list[list[str]] = [["C"], ["M"], ["P", "D", "N", "Z"]]
-        self.assertEqual(EXPECT, day5(DATA, MOVES))
+        self.assertEqual(EXPECT, day5(data, MOVES))

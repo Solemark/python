39f037be3ab806c44ceecf22565ed5aebb0c7c16
Solemark <larco9mason@gmail.com>
2024-03-15 21:38:31 +1000

    simpify code by removing ternary

--- advent_of_code_2022/day1.py
+++ advent_of_code_2022/day1.py
@@ -1,6 +1,9 @@
 def day1(data: list[str], totals: list[int] = [], total: int = 0, i: int = 0) -> str:
     if len(data) - 1 <= i:
         return f"Elf {max(i for i in enumerate([*totals, int(data[i])]))[0]} has the most calories"
-    totals = [*totals, total] if data[i] == "" else totals
-    total = 0 if data[i] == "" else (total + int(data[i]))
+    if data[i] == "":
+        totals = [*totals, total]
+        total = 0
+    else:
+        total = total + int(data[i])
     return day1(data, totals, total, i + 1)

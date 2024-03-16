b0eeb533fe378025f7a2c339818005140bf90341
Solemark <larco9mason@gmail.com>
2024-03-16 18:48:16 +1000

    make day1 more readable

--- advent_of_code_2022/day1.py
+++ advent_of_code_2022/day1.py
@@ -1,9 +1,13 @@
 def day1(data: list[str], totals: list[int] = [], total: int = 0, i: int = 0) -> str:
     if len(data) - 1 <= i:
-        return f"Elf {max(i for i in enumerate([*totals, int(data[i])]))[0]} has the most calories"
+        return f"Elf {__getMax([*totals, total])} has the most calories"
     if data[i] == "":
         totals = [*totals, total]
         total = 0
     else:
         total = total + int(data[i])
     return day1(data, totals, total, i + 1)
+
+
+def __getMax(totals: list[int]) -> int:
+    return max(i for i, _ in enumerate(totals))

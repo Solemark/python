1f455893b17b6b46158023626591f87ed91c9735
Solemark <larco9mason@gmail.com>
2024-03-22 20:57:21 +1000

    book & movie server MVP

--- book_and_movie_server/book_server.py
+++ book_and_movie_server/book_server.py
@@ -7,32 +7,26 @@ from book import Book
 app = Flask(__name__)
 
 
-@app.get("/books/<book>")
-def get_book(book: str) -> list[dict[str, str | float]]:
-    """Get a specific book from the server"""
-    books: list[Book] = __get_book_list()
-    return [__serialize(b) for b in books if b.get_name() == escape(book)]
-
-
 @app.get("/books")
 def get_books() -> list[dict[str, str | float]]:
     """Get the entire book list from the server"""
     books: list[Book] = __get_book_list()
-    return [__serialize(b) for b in books]
+    return [b.serialize() for b in books]
 
 
-"""
+@app.get("/books/<book>")
+def get_book(book: str) -> list[dict[str, str | float]]:
+    """Get a specific book from the server"""
+    books: list[Book] = __get_book_list()
+    return [b.serialize() for b in books if b.get_name() == escape(book)]
+
+
 @app.post("/books/save")
-def post_books() -> Any:
-    res: Any = request.json  # request.json() ???
+def save_book() -> Any:
+    """Save a new book to the DB"""
+    res: Any = request.json
     books: list[Book] = [*__get_book_list(), res]
     __save_book_list(books)
-"""
-
-
-def __serialize(b: Book) -> dict[str, str | float]:
-    """Serialize a book to be sent accross the network"""
-    return {"name": b.get_name(), "quantity": b.get_quantity(), "price": b.get_price()}
 
 
 def __get_book_list() -> list[Book]:
@@ -45,9 +39,8 @@ def __get_book_list() -> list[Book]:
     return books
 
 
-"""
 def __save_book_list(books: list[Book]) -> None:
+    """Save the Book list"""
     f: Any = open("data/books.csv", "w")
     for book in books:
         f.write(book.__str__())
-"""
---
+++ book_and_movie_server/movie_server.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+from markupsafe import escape
+from typing import Any
+
+from movie import Movie
+
+app = Flask(__name__)
+
+
+@app.get("/movies")
+def get_movies() -> list[dict[str, str | float]]:
+    """Get the entire movie list from the server"""
+    movies: list[Movie] = __get_movie_list()
+    return [m.serialize() for m in movies]
+
+
+@app.get("/movies/<movie>")
+def get_movie(movie: str) -> list[dict[str, str | float]]:
+    """Get a specific movie from the server"""
+    movies: list[Movie] = __get_movie_list()
+    return [m.serialize() for m in movies if m.get_name() == escape(movie)]
+
+
+@app.post("/movies/save")
+def save_movie() -> Any:
+    """Save a new movie to the DB"""
+    res: Any = request.json
+    movies: list[Movie] = [*__get_movie_list(), res]
+    __save_movie_list(movies)
+
+
+def __get_movie_list() -> list[Movie]:
+    """Get the list of movies from file"""
+    m = open("data/movies.csv").readlines()
+    movies: list[Movie] = []
+    for i in m:
+        j: list[str] = i.split(",")
+        movies = [*movies, Movie(j[0], float(j[1]), float(j[2]))]
+    return movies
+
+
+def __save_movie_list(movies: list[Movie]) -> None:
+    """Save the Movie list"""
+    f: Any = open("data/movies.csv", "w")
+    for movie in movies:
+        f.write(movie.__str__())

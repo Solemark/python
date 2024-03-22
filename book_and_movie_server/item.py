afe6bbc86b96c6b8d1ffd3b7578f02ce8dc3e334
Solemark <larco9mason@gmail.com>
2024-03-22 20:57:01 +1000

    add serialize() to item

--- book_and_movie_server/item.py
+++ book_and_movie_server/item.py
@@ -15,39 +15,48 @@ class Item:
         self.__total_bill = (quantity * price) + self.__tax_cost
 
     def set_name(self, v: str) -> None:
-        """Set the BookOrder name"""
+        """Set the item name"""
         self.__name = v
 
     def get_name(self) -> str:
-        """Get the BookOrder name"""
+        """Get the item name"""
         return self.__name
 
     def set_quantity(self, v: float) -> None:
-        """Set the BookOrder quantity"""
+        """Set the item quantity"""
         self.__quantity = v
 
     def get_quantity(self) -> float:
-        """Get the BookOrder quantity"""
+        """Get the item quantity"""
         return self.__quantity
 
     def set_price(self, v: float) -> None:
-        """Set the BookOrder price"""
+        """Set the item price"""
         self.__price = v
 
     def get_price(self) -> float:
-        """Get the BookOrder price"""
+        """Get the item price"""
         return self.__price
 
     def set_tax(self, v: float) -> None:
+        """Set the item tax"""
         self.__tax = v
 
     def get_tax(self) -> float:
-        """Get the BookOrder tax"""
+        """Get the item tax"""
         return self.__tax
 
     def get_result(self) -> float:
-        """Get the BookOrder result"""
+        """Get the item result"""
         return self.__total_bill
+
+    def serialize(self) -> dict[str, str | float]:
+        """Serialize the item to be sent across the network"""
+        return {
+            "name": self.__name,
+            "quantity": self.__quantity,
+            "price": self.__price,
+        }
 
     def __str__(self) -> str:
         return f"{self.__name},{self.__quantity},{self.__price}"

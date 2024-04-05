3b6424e32a032c4fafbf4d2b250c76530e24fefe
Solemark <larco9mason@gmail.com>
2024-04-05 17:20:00 +1000

    seperate loading functions into own file

--- twenty_questions/__init__.py
+++ twenty_questions/__init__.py
@@ -1,59 +1,8 @@
 from random import randint
 
 from character import Character
-from question import Question, FType, QType
-
-
-def __load_characters() -> list[Character]:
-    return [
-        Character(
-            "Timmy",
-            "blonde",
-            "brown",
-            "white",
-            "short",
-            "thin",
-            "red",
-            "blue",
-        )
-    ]
-
-
-def __load_questions() -> list[list[Question]]:
-    return [
-        [
-            Question(FType.HAVE, "blonde hair", QType.HAIR),
-            Question(FType.HAVE, "brown hair", QType.HAIR),
-            Question(FType.HAVE, "black hair", QType.HAIR),
-        ],
-        [
-            Question(FType.HAVE, "brown eyes", QType.EYES),
-            Question(FType.HAVE, "blue eyes", QType.EYES),
-            Question(FType.HAVE, "black eyes", QType.EYES),
-        ],
-        [
-            Question(FType.HAVE, "white skin", QType.SKIN),
-            Question(FType.HAVE, "dark skin", QType.SKIN),
-        ],
-        [
-            Question(FType.IS, "short", QType.HEIGHT),
-            Question(FType.IS, "tall", QType.HEIGHT),
-        ],
-        [
-            Question(FType.IS, "thin", QType.WEIGHT),
-            Question(FType.IS, "fat", QType.WEIGHT),
-        ],
-        [
-            Question(FType.WEAR, "red shirts", QType.SHIRT),
-            Question(FType.WEAR, "green shirts", QType.SHIRT),
-            Question(FType.WEAR, "blue shirts", QType.SHIRT),
-        ],
-        [
-            Question(FType.WEAR, "red pants", QType.PANTS),
-            Question(FType.WEAR, "green pants", QType.PANTS),
-            Question(FType.WEAR, "blue pants", QType.PANTS),
-        ],
-    ]
+from question import Question
+from data_loader import load_characters, load_questions
 
 
 def __roll(q: list[Question]) -> Question:
@@ -63,8 +12,8 @@ def __roll(q: list[Question]) -> Question:
 
 
 def main() -> None:
-    characters: list[Character] = __load_characters()
-    questions: list[list[Question]] = __load_questions()
+    characters: list[Character] = load_characters()
+    questions: list[list[Question]] = load_questions()
     asked: list[str] = []
     for Q in questions:
         q: Question = __roll(Q)
---
+++ twenty_questions/data_loader.py
@@ -0,0 +1,54 @@
+from character import Character
+from question import Question, FType, QType
+
+
+def load_characters() -> list[Character]:
+    return [
+        Character(
+            "Timmy",
+            "blonde",
+            "brown",
+            "white",
+            "short",
+            "thin",
+            "red",
+            "blue",
+        )
+    ]
+
+
+def load_questions() -> list[list[Question]]:
+    return [
+        [
+            Question(FType.HAVE, "blonde hair", QType.HAIR),
+            Question(FType.HAVE, "brown hair", QType.HAIR),
+            Question(FType.HAVE, "black hair", QType.HAIR),
+        ],
+        [
+            Question(FType.HAVE, "brown eyes", QType.EYES),
+            Question(FType.HAVE, "blue eyes", QType.EYES),
+            Question(FType.HAVE, "black eyes", QType.EYES),
+        ],
+        [
+            Question(FType.HAVE, "white skin", QType.SKIN),
+            Question(FType.HAVE, "dark skin", QType.SKIN),
+        ],
+        [
+            Question(FType.IS, "short", QType.HEIGHT),
+            Question(FType.IS, "tall", QType.HEIGHT),
+        ],
+        [
+            Question(FType.IS, "thin", QType.WEIGHT),
+            Question(FType.IS, "fat", QType.WEIGHT),
+        ],
+        [
+            Question(FType.WEAR, "red shirts", QType.SHIRT),
+            Question(FType.WEAR, "green shirts", QType.SHIRT),
+            Question(FType.WEAR, "blue shirts", QType.SHIRT),
+        ],
+        [
+            Question(FType.WEAR, "red pants", QType.PANTS),
+            Question(FType.WEAR, "green pants", QType.PANTS),
+            Question(FType.WEAR, "blue pants", QType.PANTS),
+        ],
+    ]

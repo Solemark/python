a7ce3b2394c7699510c4883ab24b5d55ea75d466
Solemark <larco9mason@gmail.com>
2024-04-05 16:00:49 +1000

    added get_form, question & value

--- twenty_questions/question.py
+++ twenty_questions/question.py
@@ -3,7 +3,7 @@ class Question:
         self.__form: int = form
         self.__question: str = question
 
-    def __question_prefix(self) -> str:
+    def question_prefix(self) -> str:
         match self.__form:
             case 0:
                 return "does your character have"
@@ -12,5 +12,15 @@ class Question:
             case 2:
                 return "Does your character wear"
 
+    def get_form(self) -> int:
+        return self.__form
+
+    def get_question(self) -> str:
+        return self.__question
+
+    def get_value(self) -> str:
+        val: str = self.__question.split(" ")
+        return val[0] if len(val) > 1 else self.__question
+
     def __str__(self) -> str:
-        return f"{self.__question_prefix()} {self.__question}"
+        return f"{self.question_prefix()} {self.__question}"

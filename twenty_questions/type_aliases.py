ee98c7ca2ccf751540cfa88c626a00d26f3a2da3
Solemark <larco9mason@gmail.com>
2024-04-06 19:04:35 +1000

    seperated TypeAlias' into own class

---
+++ twenty_questions/type_aliases.py
@@ -0,0 +1,11 @@
+from typing import TypeAlias
+
+from character import Character
+from question import Question
+
+
+CLIST: TypeAlias = list[Character]
+QLIST: TypeAlias = list[list[Question]]
+ALIST: TypeAlias = list[str]
+CH: TypeAlias = Character
+QU: TypeAlias = Question

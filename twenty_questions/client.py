3261b2da0f17d3c268484c25977e584daa7fe818
Solemark <larco9mason@gmail.com>
2024-04-07 11:33:36 +1000

    refactor to be more readable

--- twenty_questions/client.py
+++ twenty_questions/client.py
@@ -4,28 +4,16 @@ from data_loader import get_data
 from stub import CList, QList, AList, Ch, Qu
 
 
-def __guess(CL: CList, char: Ch, i: int = 1) -> Ch:
-    """perform a guess by finding the character with the highest weighting"""
-    if i > len(CL) - 1:
-        return char
-    if CL[i].get_weighting() > char.get_weighting():
-        return __guess(CL, CL[i], i + 1)
-    return __guess(CL, char, i + 1)
-
-
-def __roll(q: list[Qu]) -> Qu:
-    """Determine which question in a category to ask"""
-    M: int = len(q) - 1
-    i: int = randint(0, M)
-    return q[i]
-
-
-def __search_traits(CL: CList, Q: Qu, i: int = 0) -> CList:
-    """Search for Characters with the Trait"""
-    if i > len(CL) - 1:
-        return CL
-    CL[i].search_trait(Q.get_type(), Q.get_value())
-    return __search_traits(CL, Q, i + 1)
+def main() -> None:
+    """Watch the computer play 20 questions with itself!"""
+    CL, QL = get_data()
+    CL, AL = __ask(CL, QL)
+    char: Ch = __guess(CL, CL[0])
+    if char.get_weighting() == 0:
+        """No correct questions! Pretend it didnt happen!"""
+        main()
+    print(f"{char.get_name()}: {char.get_weighting()}")
+    print(f"questions asked: {AL}")
 
 
 def __ask(CL: CList, QL: QList, AL: AList = [], i: int = 0) -> tuple[CList, AList]:
@@ -37,16 +25,28 @@ def __ask(CL: CList, QL: QList, AL: AList = [], i: int = 0) -> tuple[CList, ALis
     return __ask(CL, QL, [*AL, Q.__str__()], i + 1)
 
 
-def main() -> None:
-    """Watch the computer play 20 questions with itself!"""
-    CL, QL = get_data()
-    CL, AL = __ask(CL, QL)
-    char: Ch = __guess(CL, CL[0])
-    if char.get_weighting() == 0:
-        """No correct questions! Pretend it didnt happen!"""
-        main()
-    print(f"{char.get_name()}: {char.get_weighting()}")
-    print(f"questions asked: {AL}")
+def __search_traits(CL: CList, Q: Qu, i: int = 0) -> CList:
+    """Search for Characters with the Trait"""
+    if i > len(CL) - 1:
+        return CL
+    CL[i].search_trait(Q.get_type(), Q.get_value())
+    return __search_traits(CL, Q, i + 1)
+
+
+def __roll(q: list[Qu]) -> Qu:
+    """Determine which question in a category to ask"""
+    M: int = len(q) - 1
+    i: int = randint(0, M)
+    return q[i]
+
+
+def __guess(CL: CList, char: Ch, i: int = 1) -> Ch:
+    """perform a guess by finding the character with the highest weighting"""
+    if i > len(CL) - 1:
+        return char
+    if CL[i].get_weighting() > char.get_weighting():
+        return __guess(CL, CL[i], i + 1)
+    return __guess(CL, char, i + 1)
 
 
 if __name__ == "__main__":

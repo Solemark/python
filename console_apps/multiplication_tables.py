d208c489078b9a1534df6b07b9a42e46b38fdaa9
Solemark <larco9mason@gmail.com>
2024-10-06 17:11:10 +1000

    cleanup multiplication_tables.py

--- console_apps/multiplication_tables.py
+++ console_apps/multiplication_tables.py
@@ -1,6 +1,2 @@
 def calculate(table: int, max: int) -> list[str]:
-    return [__format_data(table, x) for x in range(max + 1)]
-
-
-def __format_data(table: int, number: int) -> str:
-    return f"{str(table)} x {str(number)} = {str(table * number)}"
+    return [f"{str(table)} x {str(i)} = {str(table*i)}" for i in range(max + 1)]
